8b89674e3e454a1701af63b30f066d57005c7063
nikhil <nikhiltr04@gmail.com>
2026-05-04 13:00:48 +0530

    phase 1 step 4: global error handling + async wrapper

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { errorHandler } from "./middleware/error.middleware";
 
 const app = express();
 
@@ -11,5 +12,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use(errorHandler);
 
 export default app;
